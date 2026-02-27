064752cae56848500c2bea47f2e9ebabfdcff22e
enseitankado <ozgurkoca@gmail.com>
2026-02-28 02:42:47 +0300

    -

--- README.md
+++
@@ -1,4 +0,0 @@
-
-ETAP geliştirmelerini burada takip ediyorum. 
-
-Araştırma, not ve araçlarımı kendi dizinlerinde bulabilirsiniz.
--- usr/share/eta/eta-right-click/util.py
+++
@@ -1,9 +0,0 @@
-import threading
-
-def asynchronous(func):
-    def wrapper(*args, **kwargs):
-        thread = threading.Thread(target=func, args=args, kwargs=kwargs)
-        thread.daemon = True
-        thread.start()
-        return thread
-    return wrapper
