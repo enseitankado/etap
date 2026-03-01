1c11c9caf820c40ca93c72d73856f5088294dedc
enseitankado <ozgurkoca@gmail.com>
2026-03-02 00:04:43 +0300

    Pardus-23.4-ETAP-20260216-1444-amd64

--- usr/share/eta/eta-qr-login/unix_socket_service.py
+++ usr/share/eta/eta-qr-login/unix_socket_service.py
@@ -1,5 +1,6 @@
 import socket
 import os
+import pwd
 import json
 import threading
 import sys
@@ -21,8 +22,8 @@ class UnixSocketService:
             if os.path.exists(self.SOCKET_PATH):
                 os.remove(self.SOCKET_PATH)
         except Exception as e:
-            print(traceback.format_exc())
-            print(f"(unix_socket) Socket cleanup error: {e}")
+            print(traceback.format_exc(), file=sys.stderr)
+            print(f"(unix_socket) Socket cleanup error: {e}", file=sys.stderr)
 
     def handle_client(self, connection):
         """ Handle client requests """
@@ -32,8 +33,8 @@ class UnixSocketService:
             if self.event:
                 self.event(request)
         except Exception as e:
-            print(traceback.format_exc())
-            print(f"(unix_socket) Client handling error: {e}")
+            print(traceback.format_exc(), file=sys.stderr)
+            print(f"(unix_socket) Client handling error: {e}", file=sys.stderr)
         finally:
             connection.close()
 
@@ -46,18 +47,21 @@ class UnixSocketService:
             self.server.bind(self.SOCKET_PATH)
             self.server.listen(5)
 
-            os.chmod(self.SOCKET_PATH, 0o1777)
+            lightdm_info = pwd.getpwnam("lightdm")
+            lightdm_uid = lightdm_info.pw_uid
+            os.chmod(self.SOCKET_PATH, 0o700)
+            os.chown(self.SOCKET_PATH, lightdm_uid, -1)
 
             print(f"(unix_socket) Running at {self.SOCKET_PATH}", file=sys.stderr)
 
             while self.running:
                 conn, _ = self.server.accept()
-                print(f"(unix_socket) New Connection: {conn}")
+                print(f"(unix_socket) New Connection: {conn}", file=sys.stderr)
                 threading.Thread(target=self.handle_client, args=(conn,)).start()
 
         except Exception as e:
-            print(traceback.format_exc())
-            print(f"(unix_socket) Server error: {e}")
+            print(traceback.format_exc(), file=sys.stderr)
+            print(f"(unix_socket) Server error: {e}", file=sys.stderr)
 
         finally:
             self.cleanup_socket()
